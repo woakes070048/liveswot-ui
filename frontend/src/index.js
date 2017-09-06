faaf7acc8773e8799406533dfc47d7fd1dd04766
Imran Ariffin <ariffinimran@gmail.com>
2017-09-06 16:50:21 -0400

    success removing flux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppContainer from './containers/AppContainer';
+import App from './views/App.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<AppContainer />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 registerServiceWorker();
