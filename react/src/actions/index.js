b53976e7fddeb052b239a4472fafdb988e1262cf
Imran Ariffin <ariffinimran@gmail.com>
2018-02-12 23:26:28 -0500

    init-redux: add item, vote item, add unit tests

--- react/src/actions/index.js
+++ react/src/actions/index.js
@@ -4,6 +4,6 @@ export const AddItem = (text, boardType) => {
   return { type: ADD_ITEM, text, boardType };
 };
 
-export const VoteItem = () => {
-  return { type: VOTE_ITEM };
+export const VoteItem = (id) => {
+  return { type: VOTE_ITEM, id };
 };
--- react/src/reducers/items.js
+++ react/src/reducers/items.js
@@ -1,12 +1,28 @@
 
-import { ADD_ITEM } from '../actions/actionTypes';
+import { ADD_ITEM, VOTE_ITEM } from '../actions/actionTypes';
 
 const items = (state = [], action) => {
   switch (action.type) {
+
     case ADD_ITEM:
       const { text, boardType } = action;
-      const id = `${state.length}`;
-      return [ ...state, { id, text, boardType } ];
+      const newId = state.length;
+      return [ ...state, { id: newId, text, boardType, vote: 0 } ];
+
+    case VOTE_ITEM:
+      const { id: voteId } = action;
+      const newState = state.map((item) => {
+        return item.id === voteId
+          ? { ...item, vote: item.vote + 1 }
+          : { ...item };
+      });
+
+      newState.sort((a, b) => {
+        return b.vote - a.vote;
+      });
+
+      return newState;
+
     default:
       return state;
   }
