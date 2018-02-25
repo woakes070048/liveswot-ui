cd8bf89dae0eccee2e9e17f8debf8bdff3dd602f
Imran Ariffin <ariffinimran@gmail.com>
2018-02-25 11:53:07 -0500

    #15: create login page and configure react routes

---
+++ react/src/middlewares/history.js
@@ -0,0 +1,6 @@
+import createHistory from 'history/createBrowserHistory';
+import { routerMiddleware } from 'react-router-redux';
+
+export const history = createHistory();
+
+export default routerMiddleware(history);
--- react/src/reducers/index.js
+++ react/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+
 import items from './items';
 import newItem from './newItem';
+import { routerReducer } from 'react-router-redux';
 
 export default combineReducers({
   items,
   newItem,
+  router: routerReducer,
 });
