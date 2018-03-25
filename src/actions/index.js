c41baf9874836427d788c7f4d044da5322a2dfcf
Imran Ariffin <ariffinimran@gmail.com>
2018-03-25 02:11:08 -0400

    #30: add item async api action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,32 +1,40 @@
 import {
-  ADD_ITEM,
   VOTE_ITEM,
   UPDATE_NEW_ITEM,
   CLEAR_NEW_ITEM,
   CALL_API,
   FETCH_ITEMS, FETCH_ITEMS_SUCCESS, FETCH_ITEMS_ERROR,
   LOGIN, LOGIN_SUCCESS, LOGIN_ERROR,
-  SIGNUP, SIGNUP_SUCCESS, SIGNUP_ERROR, LOGOUT, LOGOUT_SUCCESS, LOGOUT_ERROR, LOAD_TOKEN, INIT_APP
+  SIGNUP, SIGNUP_SUCCESS, SIGNUP_ERROR,
+  LOGOUT, LOGOUT_SUCCESS, LOGOUT_ERROR,
+  LOAD_TOKEN, INIT_APP,
+  ADD_ITEM, ADD_ITEM_SUCCESS, ADD_ITEM_ERROR
 } from './actionTypes';
 
 export const InitApp = () => {
-  return { type: INIT_APP };
+  return {type: INIT_APP};
 };
 
 export const AddItem = (text, cardType) => {
-  return { type: ADD_ITEM, text, cardType };
+  return {
+    type: CALL_API,
+    types: [ADD_ITEM, ADD_ITEM_SUCCESS, ADD_ITEM_ERROR],
+    method: 'POST',
+    endpoint: '/swot-item/',
+    data: {text, cardType},
+  };
 };
 
 export const VoteItem = (id) => {
-  return { type: VOTE_ITEM, id };
+  return {type: VOTE_ITEM, id};
 };
 
 export const UpdateNewItem = (text, cardType) => {
-  return { type: UPDATE_NEW_ITEM, text, cardType };
+  return {type: UPDATE_NEW_ITEM, text, cardType};
 };
 
 export const ClearNewItem = (cardType) => {
-  return { type: CLEAR_NEW_ITEM, cardType };
+  return {type: CLEAR_NEW_ITEM, cardType};
 };
 
 export const FetchItems = () => {
@@ -43,7 +51,7 @@ export const Signup = (email, username, password) => {
     method: 'POST',
     endpoint: '/auth/signup',
     types: [SIGNUP, SIGNUP_SUCCESS, SIGNUP_ERROR],
-    data: { user: { email, username, password }},
+    data: {user: {email, username, password}},
   };
 };
 
@@ -53,18 +61,18 @@ export const Login = (email, password) => {
     method: 'POST',
     endpoint: '/auth/login',
     types: [LOGIN, LOGIN_SUCCESS, LOGIN_ERROR],
-    data: { user: { email, password }},
+    data: {user: {email, password}},
   };
 };
 
 export const Logout = () => { return { type: LOGOUT, }; };
 
 export const LogoutSuccess = (token) => {
-  return { type: LOGOUT_SUCCESS, data: {token }};
+  return {type: LOGOUT_SUCCESS, data: {token}};
 };
 
 export const LogoutError = () => { return { type: LOGOUT_ERROR, }; };
 
 export const LoadToken = (token) => {
-  return { type: LOAD_TOKEN, data: {token }};
+  return {type: LOAD_TOKEN, data: {token}};
 };
