f8ea692b55aad4cf0d68c71c94f621e462570bf3
Imran Ariffin <ariffinimran@gmail.com>
2018-04-08 17:57:45 -0400

    41: add more test to swot selector

--- src/selectors/swots/getSwots.test.js
+++ src/selectors/swots/getSwots.test.js
@@ -39,4 +39,29 @@ describe('(selectors) getSwots', () => {
       expect(swots[0]).to.deep.equal(swot);
     });
   });
+
+  describe('state has more than 1 swot', () => {
+    const
+      swot1 = {swotId: 1, creatorId: 101, title: 'Title 1', description: 'Description 1'},
+      swot2 = {swotId: 2, creatorId: 102, title: 'Title 2', description: 'Description 2'},
+      swot3 = {swotId: 3, creatorId: 101, title: 'Title 3', description: 'Description 3'};
+    const state = {
+      swots: {
+        byId: {
+          1: swot1,
+          2: swot2,
+          3: swot3,
+        },
+        errors: [],
+        isLoading: false,
+      }
+    };
+
+    it('returns array with correct swots', () => {
+      const swots = getSwots(state);
+      expect(swots).to.be.an('array');
+      expect(swots.length).to.equal(3);
+      expect(swots).to.deep.equal([swot1, swot2, swot3]);
+    });
+  });
 });
