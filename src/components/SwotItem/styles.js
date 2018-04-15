5e5a9bf9c470a9e5c34464f79519bd0236ae178c
Imran Ariffin <ariffinimran@gmail.com>
2018-04-15 15:23:43 -0400

    #40: swot item partial border

--- src/components/SwotItem/styles.js
+++ src/components/SwotItem/styles.js
@@ -1,13 +1,18 @@
 
 const borderColor = '#e6e6e6';
+const widthPercent = 96;
+const marginLeftPercent = (100 - widthPercent) / 2;
+const marginRightPercent = marginLeftPercent;
 
 const swotItemStyles = (index) => ({
   backgroundColor: '#',
-  width: '100%',
+  width: `${widthPercent}%`,
   minHeight: '50px',
   borderTop: index === 0 ? `1px solid ${borderColor}` : '',
   borderBottom: `1px solid ${borderColor}`,
   padding: `0.5rem 0.5rem`,
+  marginLeft: `${marginLeftPercent}%`,
+  marginRight: `${marginRightPercent}%`,
 });
 
 export default swotItemStyles;
