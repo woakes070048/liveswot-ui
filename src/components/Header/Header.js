5632f8a46209c363ae35d4c8866b90908b5ab25f
Imran Ariffin <ariffinimran@gmail.com>
2018-05-20 15:00:37 -0400

    #56: fix logout/login button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,7 +17,7 @@ const Header = ({logout, username, email, profileImg, isLoggedIn}) => {
               <a href='/' className='brand-logo'>{localizedText().title}</a>
               <ul id='nav-mobile' className='right hide-on-med-and-down'>
                 {!isLoggedIn && <li><a href='/login'>Login</a></li>}
-                {isLoggedIn && <li><a href='!#' onClick={() => logout()}>Logout</a></li>}
+                {isLoggedIn && <li><a href='' onClick={() => logout()}>Logout</a></li>}
                 {isLoggedIn &&
                 <li style={styles.navBarList}>
                   <a href='/profile'>
