4d16ef501a16827a240c22afa9871f3e231a17c6
Imran Ariffin <ariffinimran@gmail.com>
2018-05-21 00:14:07 -0400

    #58: `RequestButton` remove bottom border on `isLoading`

--- src/components/RequestButton/index.js
+++ src/components/RequestButton/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-
+import './styles.css';
 import styles from './styles';
 
 const RequestButton = ({text, onClick, requestedItem}) => {
   const disabled = requestedItem.isLoading ? 'disabled' : '';
   return (
-    <a
-      className={`waves-effect waves-light btn ${disabled}`}
+    <input
+      type={`submit`}
+      className={`waves-effect waves-light btn no-border ${disabled}`}
       style={styles.button(requestedItem)}
       onClick={onClick}
-    >
-      {text}
-    </a>
+      value={text}
+    />
   );
 };
 
