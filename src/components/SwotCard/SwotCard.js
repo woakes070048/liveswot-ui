38d34f03e45ef41b7d5718effda3746abad54f20
Imran Ariffin <ariffinimran@gmail.com>
2018-06-10 18:51:41 -0400

    #67: `SwotItem` transition animation on vote

--- src/components/SwotCard/SwotCard.js
+++ src/components/SwotCard/SwotCard.js
@@ -6,57 +6,75 @@ import {localizedText} from '../../utils/index';
 import styles from './styles';
 import './style.css';
 
+const DURATION_INVISIBLE = 300;
 
-const SwotCard = ({
-                    user,
-                    swotId,
-                    items,
-                    text,
-                    cardType,
-                    onChange,
-                    onSubmit,
-}) => {
+class SwotCard extends React.Component {
 
-  return (
-    <div>
-      <form method="POST" onSubmit={ (e) => {
-        e.preventDefault();
-        onSubmit(swotId, text, cardType);
-      } }>
-        <div className='input-field' style={styles.inputField}>
-          <input
-            id={`input-${cardType}`}
-            className='validate'
-            style={styles.input}
-            value={text}
-            type='text'
-            onChange={onChange}
-          />
-          <label
-            className={ text ? 'active' : ''}
-            htmlFor={`input-${cardType}`}
-          >
-            {`Add ${localizedText().swot.cardType[cardType]}`}
-          </label>
-        </div>
-      </form>
-      <div style={styles.cardPanelWrapper}>
-        <div className={`card-panel`} style={styles.cardPanel}>
-          {
-            (items.length > 0 &&
-              (<ul style={styles.swotList}>{
+  constructor(props) {
+    super(props);
+    this.state = {hidden: false};
+    this.animate = this.animate.bind(this);
+  }
+
+  animate() {
+    this.setState({hidden: true});
+    setTimeout(() => {
+      this.setState({hidden: false});
+    }, DURATION_INVISIBLE);
+  }
+
+  render() {
+    const {swotId, items, text, cardType, onChange, onSubmit} = this.props;
+
+    return (
+      <div>
+        <form method="POST" onSubmit={ (e) => {
+          e.preventDefault();
+          onSubmit(swotId, text, cardType);
+        } }>
+          <div className='input-field' style={styles.inputField}>
+            <input
+              id={`input-${cardType}`}
+              className='validate'
+              style={styles.input}
+              value={text}
+              type='text'
+              onChange={onChange}
+            />
+            <label
+              className={ text ? 'active' : ''}
+              htmlFor={`input-${cardType}`}
+            >
+              {`Add ${localizedText().swot.cardType[cardType]}`}
+            </label>
+          </div>
+        </form>
+        <div style={styles.cardPanelWrapper}>
+          <div className={`card-panel`} style={styles.cardPanel}>
+            {
+              (items.length > 0 &&
+                (<ul style={styles.swotList}>{
                   items.map((item, i) => {
-                    return (<SwotItem swotItem={item} key={i} index={i}/>);
+                    return (
+                      <SwotItem
+                        hidden={this.state.hidden}
+                        animate={this.animate}
+                        swotItem={item}
+                        key={i}
+                        index={i}
+                      />
+                    );
                   })
-              }</ul>)
-            )
-            || <h1>{localizedText().swot.cardType[cardType]}</h1>
-          }
+                }</ul>)
+              )
+              || <h1>{localizedText().swot.cardType[cardType]}</h1>
+            }
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 SwotCard.propTypes = {
   swotId: PropTypes.number.isRequired,
--- src/components/SwotItem/SwotItem.js
+++ src/components/SwotItem/SwotItem.js
@@ -1,40 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {findDOMNode} from 'react-dom';
 
 import styles from './styles';
 import './styles.css';
 import VoteButton from '../VoteButton';
 
 
-const SwotItem = ({swotItem, votes, index}) => {
-  const profileImg = 'https://scontent.fyto1-1.fna.fbcdn.net/v/t1.0-9/25507801_10214600576038909_8129308682006032833_n.jpg?oh=f6a69fa4bb09fa9a11b1e87c176dc732&oe=5B382481';
+class SwotItem extends React.Component {
 
-  return (
-    <li style={styles.swotItem(index)} className='swot-item'>
-      <div className={'row'} style={styles.swotItemRow}>
-        <div className={'col s1 m1 l1'} style={styles.left}>
-          <div style={styles.creatorImgWrapper}>
-            <img
-              className={`circle img-responsive`}
-              src={profileImg}
-              style={styles.creatorImg}
-              alt={`item creator: ${swotItem.creatorId}`}
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const profileImg = 'https://scontent.fyto1-1.fna.fbcdn.net/v/t1.0-9/25507801_10214600576038909_8129308682006032833_n.jpg?oh=f6a69fa4bb09fa9a11b1e87c176dc732&oe=5B382481';
+    const {swotItem, votes, index, hidden, animate} = this.props;
+
+    // if (this.state.hidden) {
+    //   setTimeout(() => {
+    //     this.setState({hidden: false});
+    //   }, 1000);
+    //
+    //   return null;
+    // }
+
+    return (
+      <li style={styles.swotItem(index)} className={`swot-item ${hidden ? '' : 'show'}`}>
+        <div className={'row'} style={styles.swotItemRow}>
+          <div className={'col s1 m1 l1'} style={styles.left}>
+            <div style={styles.creatorImgWrapper}>
+              <img
+                className={`circle img-responsive`}
+                src={profileImg}
+                style={styles.creatorImg}
+                alt={`item creator: ${swotItem.creatorId}`}
+              />
+            </div>
+          </div>
+          <div className={'col s10 m10 l10'} style={styles.textColumn}>
+            <span>{`${swotItem.text}`}</span>
+          </div>
+          <div className={'col s1 m1 l1'} style={styles.voteColumn}>
+            <VoteButton
+              animate={animate}
+              score={votes}
+              swotItemId={swotItem.swotItemId}
             />
           </div>
         </div>
-        <div className={'col s10 m10 l10'} style={styles.textColumn}>
-          <span>{`${swotItem.text}`}</span>
-        </div>
-        <div className={'col s1 m1 l1'} style={styles.voteColumn}>
-          <VoteButton
-            score={votes}
-            swotItemId={swotItem.swotItemId}
-          />
-        </div>
-      </div>
-    </li>
-  );
-};
+      </li>
+    );
+  }
+}
 
 SwotItem.propTypes = {
   swotItem: PropTypes.shape({
