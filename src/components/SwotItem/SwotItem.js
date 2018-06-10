4d4cc6ed5945169e0790caeb8a3c84528ea086d4
Imran Ariffin <ariffinimran@gmail.com>
2018-06-10 17:47:14 -0400

    swot items scale on hover

--- src/components/SwotItem/SwotItem.js
+++ src/components/SwotItem/SwotItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './styles';
+import './styles.css';
 import VoteButton from '../VoteButton';
 
 
@@ -9,7 +10,7 @@ const SwotItem = ({swotItem, votes, index}) => {
   const profileImg = 'https://scontent.fyto1-1.fna.fbcdn.net/v/t1.0-9/25507801_10214600576038909_8129308682006032833_n.jpg?oh=f6a69fa4bb09fa9a11b1e87c176dc732&oe=5B382481';
 
   return (
-    <li style={styles.swotItem(index)}>
+    <li style={styles.swotItem(index)} className='swot-item'>
       <div className={'row'} style={styles.swotItemRow}>
         <div className={'col s1 m1 l1'} style={styles.left}>
           <div style={styles.creatorImgWrapper}>
