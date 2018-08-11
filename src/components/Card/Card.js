241e4da5cd2662e703f449efa5d3bba277b5ba84
Imran Ariffin <ariffin.imran@gmail.com>
2018-08-11 15:01:20 -0400

    Card: accept props.style

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './styles.scss';
 
 const Card = (props) => {
-  const {children, style} = props;
+  const {children, style = {}} = props;
   return (
     <div className={styles.root} style={style}>
       {children || null}
