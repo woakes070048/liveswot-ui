89ffe5a2f4676568502955c01290dd45b7efbfb2
Imran Ariffin <ariffin.imran@gmail.com>
2018-08-11 12:25:59 -0400

    generic component Card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './styles.scss';
+
+const Card = (props) => {
+  const {children, style} = props;
+  return (
+    <div className={styles.root} style={style}>
+      {children || null}
+    </div>
+  );
+};
+
+export default Card;
