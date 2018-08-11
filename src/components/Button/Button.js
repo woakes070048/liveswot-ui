8fa24d369000cecfa3fb91fff71c57f051b51de9
Imran Ariffin <ariffin.imran@gmail.com>
2018-08-11 15:49:18 -0400

    Button: onClick as prop

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './styles.scss';
 
 const Button = (props) => {
-  const {children, disabled, cancel} = props;
+  const {children, disabled, cancel, onClick} = props;
 
   const className = `${styles.root} ${cancel ? styles.cancel : ''}`;
 
   return (
-    <button className={className} disabled={disabled}>
+    <button
+      className={className}
+      disabled={disabled}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
+};
+
+Button.propTypes = {
+  disabled: PropTypes.bool,
+  cancel: PropTypes.bool,
+  children: PropTypes.any,
+  onClick: PropTypes.func,
 };
 
 export default Button;
