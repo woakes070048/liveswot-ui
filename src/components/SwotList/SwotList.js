e5d032618b03156d14273162034ac11873ac9bde
Imran Ariffin <ariffin.imran@gmail.com>
2018-08-12 00:35:33 -0400

    SwotList: init

--- src/components/SwotList/SwotList.js
+++ src/components/SwotList/SwotList.js
@@ -1,71 +1,18 @@
 import React from 'react';
-import styles from '../SwotHeader/styles.scss';
+import styles from './SwotList.scss';
+import Kebab from '../Kebab/';
 
 const SwotList = (props) => {
-  const swotImg = '';
-  const {swotId, title, description, swotDateCreated, creatorUserName} = props;
+  // const swotImg = '';
+  // const {swotId, title, description, swotDateCreated, creatorUserName} = props;
+
   return (
-    <div>
-      <div className={`row ${styles.root}`}>
-        <div className={`col m12 s12 l12`}>
-          <div className={`card ${styles.card}`}>
-            <div className={`card-content ${styles["card-content"]}`}>
-              <div className={`row ${styles["compact-row"]} ${styles.layout}`}>
-                <div className={styles.left}>
-                  <div className={styles["swot-img-wrapper"]}>
-                    <span>
-                      {(
-                        swotImg &&
-                        <img
-                          alt={title}
-                          className={`responsive-img ${styles["swot-img"]}`}
-                          src={swotImg}
-                        />
-                      ) || (
-                        <div className={styles["swot-img-fallback-wrapper"]}>
-                          <div className={styles["swot-img-fallback"]}>
-                            ST
-                          </div>
-                        </div>
-                      )}
-                    </span>
-                  </div>
-                </div>
-                <div className={styles.mid}>
-                  <div className={`row ${styles["compact-row"]}`}>
-                    <div className={`row swot-header ${styles["compact-row"]}`}>
-                      <div className={`col s10 m12 l12 ${styles["nav-col"]}`}>
-                        <span className={`card-title activator ${styles["card-title"]}`}>
-                          <a href={`swots/${swotId}`}>
-                            {title}
-                          </a>
-                        </span>
-                      </div>
-                    </div>
-                    <div className={`row ${styles["compact-row"]}`}>
-                      <div className={`col s12 m12 l12 ${styles["nav-col"]}`}>
-                        <span>{description}</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className={`swot-header-right ${styles.right}`}>
-                  <span className={`card-title activator ${styles["card-title"]}`}>
-                    <span className={`${styles["swot-meta"]}`}>
-                      created by {creatorUserName} at {swotDateCreated}
-                    </span>
-                    <i
-                      id={`kebab`}
-                      className={`material-icons right`}
-                      onClick={() => ({})}
-                    >
-                      more_vert
-                    </i>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
+    <div className={styles.root}>
+      <div className={styles.row}>
+        <div className={styles.creator}></div>
+        <div className={styles.content}></div>
+        <div className={styles.actions}>
+          <Kebab/>
         </div>
       </div>
     </div>
