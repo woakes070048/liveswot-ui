d638a848311f31d988ba65214ff68fb13af1d87f
Imran Ariffin <ariffin.imran@gmail.com>
2018-08-13 18:24:56 -0400

    set host based on NODE_ENV

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,1 +1,14 @@
-export const host = 'http://localhost:8000/api';
+let _host;
+
+switch (process.env.NODE_ENV) {
+  case 'production':
+    _host = 'http://liveswot-api.com/api';
+    break;
+  case 'development':
+    _host = 'http://localhost:8080/api';
+    break;
+  default:
+    _host = 'http://localhost:8080/api';
+}
+
+export const host = _host;
