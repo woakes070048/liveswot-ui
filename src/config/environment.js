53dc19721611e280e63cd4a8e5758eaea7aebe4e
Imran Ariffin <ariffin.imran@gmail.com>
2018-08-13 23:46:20 -0400

    heroku url as prod & default

--- src/config/environment.js
+++ src/config/environment.js
@@ -2,13 +2,13 @@ let _host;
 
 switch (process.env.NODE_ENV) {
   case 'production':
-    _host = 'http://liveswot-api.com/api';
+    _host = 'https://liveswot-api.herokuapp.com/api';
     break;
   case 'development':
     _host = 'http://localhost:8080/api';
     break;
   default:
-    _host = 'http://localhost:8080/api';
+    _host = 'https://liveswot-api.herokuapp.com/api';
 }
 
 export const host = _host;
